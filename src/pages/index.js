c267074a21dfeeabf94577822a5e82ec268123a0
zachjmurphy <zachjmurphy@users.noreply.github.com>
2020-12-01 10:57:01 -0500

    Updated Files

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { BackgroundDisplay, MountEverest } from "../components/style"
+
+const NTP = () => (
+  <Layout>
+    <SEO title="New Tab" titleOverride />
+  </Layout>
+)
+
+export default NTP
