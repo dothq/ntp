0e6ba247ec34e8f211d717cd027c3a769caf5741
zachjmurphy <zachjmurphy@users.noreply.github.com>
2020-12-01 11:05:03 -0500

    Updated gatsby-config & tsconfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `New Tab`,
-    description: `Compass is a feature-rich new tab page designed for speed.`,
-    author: `@dothq`,
-  },
-  proxy: {
-    prefix: "/api",
-    url: "http://localhost:3000",
+    title: 'Gatsby + Node.js (TypeScript) API',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Compass`,
-        short_name: `New Tab`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#ffffff`,
-        display: `minimal-ui`,
+        name: 'Gatsby + Node.js (TypeScript) API',
+        short_name: 'Gatsby + Node.js (TypeScript)',
+        start_url: '/',
+        icon: 'src/images/icon.png',
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
   ],
-}
+};
