704618be2346041be736ba3b906cc7d90205e3ec
Ejaz Ali <ejazali4@protonmail.ch>
2021-04-10 22:25:30 +0100

    ✨ Wallpaper downloads and shows

--- html/js/wallpaper.js
+++ html/js/wallpaper.js
@@ -2,7 +2,7 @@ const getRandomArbitrary = (min, max) => {
     return Math.random() * (max - min) + min;
 }
 
-const loadWallpaper = () => {
+const selectandloadWallpaper = () => {
     fetch('/constants/backgrounds.json')
     .then((response) => {
 
@@ -10,10 +10,37 @@ const loadWallpaper = () => {
             response.json()
             .then((backgrounds) => {
                 const backgroundNum = Math.floor(getRandomArbitrary(0, backgrounds.length));
-                console.log(backgrounds[backgroundNum]);
-            })
+                downloadWallpaper(`/images/unsplash/${backgrounds[backgroundNum].id}.jpeg`);
+            });
         } else{
             netError(response.status);
         }
-    })
+    });
 };
+
+const downloadWallpaper = (uri) => {
+    fetch(uri)
+    .then((response) => {
+
+        if (response.status == 200){
+            response.blob()
+            .then((image) => {
+                window["temp_wallpaperBlob"] = URL.createObjectURL(image);
+                placeWallpaper(window["temp_wallpaperBlob"]);
+            });
+        } else{
+            netError(response.status);
+        }
+    });
+};
+
+const placeWallpaper = (blob) => {
+    document.getElementById("resting-app").setAttribute(
+        "style",
+        `background-image: url(${blob});`
+    )
+}
+
+window.addEventListener("beforeunload", () => {
+  URL.revokeObjectURL(window["temp_wallpaperBlob"]);
+})
