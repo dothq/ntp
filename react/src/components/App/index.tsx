5c9799d30d62ff405cfd582d48989ff5755903de
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-04-11 14:43:14 +0100

    💄 Add background loader and cache

--- react/src/components/App/index.tsx
+++ react/src/components/App/index.tsx
@@ -3,11 +3,21 @@ import React from "react"
 import '../../reset.css';
 import { NTP } from "./style";
 
+import backgrounds from "../../backgrounds.json";
+
+const background = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+
 export const App = () => {
-    // constr 
+    const [ready, setReady] = React.useState(false);
+
+    const img = new Image();
+    img.src = `backgrounds/unsplash/${background.id}.jpeg`;
+    img.addEventListener("load", () => {
+        setReady(true);
+    })
 
     return (
-        <NTP>
+        <NTP backgroundSrc={background.id} ready={ready}>
             <div>testing</div>
         </NTP>
     )
